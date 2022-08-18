20b10a9cb44855d1fb32419b9aa471474ee95020
UbertoGC <84751424+UbertoGC@users.noreply.github.com>
2022-08-17 19:39:25 -0500

    Update aplicación.py

--- aplicación.py
+++ aplicación.py
@@ -7,7 +7,7 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'hardware+10'
 app.config['MYSQL_DB'] = 'flaskdb'
 
-modelo=Model(app)
+ infraestructura=Infraestructura(app)
 
 app.secret_key='mysecretkey'
 
@@ -19,6 +19,12 @@ def home():
 def login():
     session['estado']=False
     return render_template("login.html")
-
+@app.route('/error',methods=["GET","POST"])
+def ingreso():
+    if infraestructura.existencia_login(request.form['cui'],request.form['password']):
+        session['estado']=True
+        return  "Acceso permitido, buenos dias"
+    else:
+        return "Error password and CUI not match"
 if __name__=='__main__':#si el archivo que se esta ejecutando es el main es decir el app.py entonces arranca el servidor
     app.run(port=3000,debug=True)#corre el servidor
