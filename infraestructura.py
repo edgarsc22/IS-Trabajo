58f36194695aae6d8e1a5d7375b767e30806e0cf
UbertoGC <84751424+UbertoGC@users.noreply.github.com>
2022-08-17 19:35:47 -0500

    Update and rename modelo.py to infraestructura.py

---
+++ infraestructura.py
@@ -0,0 +1,15 @@
+from flask_mysqldb import MySQL
+class Infraestructura:
+    def __init__(self, app):
+        self.mysql=MySQL(app)
+        pass    
+    def existencia_login(self,cui,password):
+        usuario={'cui':cui, 'password':password}
+        query = "SELECT * FROM user WHERE cui=%(cui)s and password=%(password)s"
+        cur=self.mysql.connection.cursor()
+        cur.execute(query, usuario)
+        data=cur.fetchall()
+        if not data:
+            return False
+        else:
+            return True
