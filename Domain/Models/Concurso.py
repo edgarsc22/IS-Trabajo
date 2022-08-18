7d6975e20a26e43fc2f8d71fffbbcace2949c64e
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2022-08-17 21:00:52 -0500

    Add files via upload

---
+++ Domain/Models/Concurso.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Concurso:
+    def __init__(self):
+        self.id = None
+        self.nombre = None
+        self.cantidadParticipantes = None
+        self.basesConcurso = None
+
---
+++ Domain/Models/Usuario.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Usuario:
+    def __init__(self):
+        self.id = None
+        self.nombre = None
+        self.email = None
+        self.contraseña = None
+
+    def IniciarSesion(self, ):
+        pass
+
+    def RegistrarUsuario(self, ):
+        pass
+
+    def ParticiparEvento(self, ):
+        pass
+
+    def CalificarEvento(self, ):
+        pass
+
+    def ComentarEvento(self, ):
+        pass
+
+    def BuscarEvento(self, ):
+        pass
+
+    def CambiarStatus(self, ):
+        pass
+
